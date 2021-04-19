f93a13c589b57c529d88a5893ad088448444d9c7
Tony Spencer <mobiletony@msn.com>
2021-04-19 13:12:07 -0600

    adding in an event to monitor the redirectUri

--- Authn/Startup.cs
+++ Authn/Startup.cs
@@ -35,71 +35,76 @@ namespace Authn
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
             })
-                .AddCookie(options =>
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/login";
+                options.AccessDeniedPath = "/denied";
+                options.Events = new CookieAuthenticationEvents()
                 {
-                    options.LoginPath = "/login";
-                    options.AccessDeniedPath = "/denied";
-                    options.Events = new CookieAuthenticationEvents()
+                    OnSigningIn = async context =>
                     {
-                        OnSigningIn = async context =>
+                        var scheme = context.Properties.Items.Where(k => k.Key == ".AuthScheme").FirstOrDefault();
+                        var claim = new Claim(scheme.Key, scheme.Value);
+                        var claimsIdentity = context.Principal.Identity as ClaimsIdentity;
+                        var userService = context.HttpContext.RequestServices.GetRequiredService(typeof(UserService)) as UserService;
+                        var nameIdentifier = claimsIdentity.Claims.FirstOrDefault(m => m.Type == ClaimTypes.NameIdentifier)?.Value;
+                        if (userService != null && nameIdentifier != null)
                         {
-                            var scheme = context.Properties.Items.Where(k => k.Key == ".AuthScheme").FirstOrDefault();
-                            var claim = new Claim(scheme.Key, scheme.Value);
-                            var claimsIdentity = context.Principal.Identity as ClaimsIdentity;
-                            var userService = context.HttpContext.RequestServices.GetRequiredService(typeof(UserService)) as UserService;
-                            var nameIdentifier = claimsIdentity.Claims.FirstOrDefault(m => m.Type == ClaimTypes.NameIdentifier)?.Value;
-                            if (userService != null && nameIdentifier != null)
+                            var appUser = userService.GetUserByExternalProvider(scheme.Value, nameIdentifier);
+                            if (appUser is null)
                             {
-                                var appUser = userService.GetUserByExternalProvider(scheme.Value, nameIdentifier);
-                                if (appUser is null)
-                                {
-                                    appUser = userService.AddNewUser(scheme.Value, claimsIdentity.Claims.ToList());
-                                }
-                                foreach (var r in appUser.RoleList)
-                                {
-                                    claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, r));
-                                }
+                                appUser = userService.AddNewUser(scheme.Value, claimsIdentity.Claims.ToList());
                             }
-                            claimsIdentity.AddClaim(claim);
-                            await Task.CompletedTask;
+                            foreach (var r in appUser.RoleList)
+                            {
+                                claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, r));
+                            }
                         }
-                    };
-                })
-                .AddOpenIdConnect("google", options =>
+                        claimsIdentity.AddClaim(claim);
+                        await Task.CompletedTask;
+                    }
+                };
+            })
+            .AddOpenIdConnect("google", options =>
+            {
+                options.Authority = Configuration["GoogleOpenId:Authority"];
+                options.ClientId = Configuration["GoogleOpenId:ClientId"];
+                options.ClientSecret = Configuration["GoogleOpenId:ClientSecret"];
+                options.CallbackPath = Configuration["GoogleOpenId:CallbackPath"];
+                options.SignedOutCallbackPath = Configuration["GoogleOpenId:SignedOutCallbackPath"];
+                options.SaveTokens = false;
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    options.Authority = Configuration["GoogleOpenId:Authority"];
-                    options.ClientId = Configuration["GoogleOpenId:ClientId"];
-                    options.ClientSecret = Configuration["GoogleOpenId:ClientSecret"];
-                    options.CallbackPath = Configuration["GoogleOpenId:CallbackPath"];
-                    options.SignedOutCallbackPath = Configuration["GoogleOpenId:SignedOutCallbackPath"];
-                    options.SaveTokens = false;
-                    options.TokenValidationParameters = new TokenValidationParameters
+                    // map name claim to ClaimTypes.Name since Google doesn't provide the name claim in the ISO way.
+                    NameClaimType = "name",
+                };
+            }).AddOpenIdConnect("okta", options =>
+            {
+                options.Authority = Configuration["OktaOpenId:Authority"];
+                options.ClientId = Configuration["OktaOpenId:ClientId"];
+                options.ClientSecret = Configuration["OktaOpenId:ClientSecret"];
+                options.CallbackPath = Configuration["OktaOpenId:CallbackPath"];
+                options.SignedOutCallbackPath = Configuration["OktaOpenId:SignedOutCallbackPath"];
+                options.ResponseType = "code";
+                options.SaveTokens = false;
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Events = new OpenIdConnectEvents()
+                {
+                    OnRedirectToIdentityProvider = async (context) =>
                     {
-                        // map name claim to ClaimTypes.Name since Google doesn't provide the name claim in the ISO way.
-                        NameClaimType = "name",
-                    };
-                }).AddOpenIdConnect("okta", options =>
-                {
-                    options.Authority = Configuration["OktaOpenId:Authority"];
-                    options.ClientId = Configuration["OktaOpenId:ClientId"];
-                    options.ClientSecret = Configuration["OktaOpenId:ClientSecret"];
-                    options.CallbackPath = Configuration["OktaOpenId:CallbackPath"];
-                    options.SignedOutCallbackPath = Configuration["OktaOpenId:SignedOutCallbackPath"];
-                    options.ResponseType = "code";
-                    options.SaveTokens = false;
-                    options.Scope.Add("openid");
-                    options.Scope.Add("profile");
-                })
-                .AddFacebook("facebook", options =>
-                {
-                    options.AppId = Configuration["FBOauth:AppId"];
-                    options.AppSecret = Configuration["FBOauth:ClientSecret"];
-                    options.ClientSecret = Configuration["FBOauth:ClientSecret"];
-                    options.CallbackPath = Configuration["FBOauth:CallbackPath"];
-                });
-
-
-
+                        var redirectUri = context.ProtocolMessage.RedirectUri;
+                        await Task.CompletedTask;
+                    }
+                };
+            })
+            .AddFacebook("facebook", options =>
+            {
+                options.AppId = Configuration["FBOauth:AppId"];
+                options.AppSecret = Configuration["FBOauth:ClientSecret"];
+                options.ClientSecret = Configuration["FBOauth:ClientSecret"];
+                options.CallbackPath = Configuration["FBOauth:CallbackPath"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
