5439106b7bafb39a09722c7aefdb2d65b44b86b5
Tony Spencer <mobiletony@msn.com>
2022-04-24 11:22:52 -0600

    added LocalRedirect for returnUrl to prevent redirect attacks.

--- Authn/Controllers/AccountController.cs
+++ Authn/Controllers/AccountController.cs
@@ -66,7 +66,7 @@ namespace Authn.Controllers
                 items.Add(".AuthScheme", CookieAuthenticationDefaults.AuthenticationScheme);
                 var properties = new AuthenticationProperties(items);
                 await HttpContext.SignInAsync(claimsPrincipal, properties);
-                return Redirect(returnUrl);
+                return LocalRedirect(returnUrl);
             }
             else
             {
